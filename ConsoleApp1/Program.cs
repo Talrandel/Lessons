ed2ba155d8e7b29cb56a2c6655635b3b98c09b06
Макаров Евгений <Makarov.Evgeniy@lit.msu.ru>
2023-12-25 13:01:33 +0300

    All 10 array tasks

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,25 +1,99 @@
 ﻿using System;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace ConsoleApp1
 {
     class Program
     {
+        static Random _random = new Random();
+
         static void Main(string[] args)
         {
             // Вводить размер массива с клавиатуры
             int[] array = GetArray(20);
-
-            // Печать исходного массива
             PrintArray(array);
 
-            int[] newArray = GetArrayWithoutDuplicates(array);
 
-            // Печать результирующего массива
+            GetAverage(array);
+
+            GetArrayAbs100(array);
+
+            GetArrayWithoutDuplicates(array);
+
+            GetAndPrintTwoDimensionalArray();
+
+            GetAndPrintTwoDimensionalArray_Line();
+
+            GetAndPrintTwoDimensionalArray_EvenLines();
+
+            GetAndPrintJaggedArray_Console();
+
+            GetAndPrintJaggedArray_ReplaceEvenValues();
+        }
+
+        /// <summary>
+        /// Задание один.
+        /// </summary>
+        /// <param name="array"></param>
+        /// <returns></returns>
+        private static void GetAverage(int[] array)
+        {
+            Console.WriteLine("\nЗадание один");
+            double sum = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                sum += array[i];
+            }
+            Console.WriteLine($"Среднее значение элементов массива: {sum / array.Length}");
+        }
+
+        /// <summary>
+        /// Задание два.
+        /// </summary>
+        /// <param name="array"></param>
+        /// <returns></returns>
+        private static void GetArrayAbs100(int[] array)
+        {
+            Console.WriteLine("\nЗадание два");
+
+            int counter = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (Math.Abs(array[i]) < 100)
+                {
+                    counter++;
+                }
+            }
+
+            int[] newArray = new int[counter];
+            counter = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (Math.Abs(array[i]) < 100)
+                {
+                    newArray[counter++] = array[i];
+                }
+            }
+
             PrintArray(newArray);
         }
 
-        private static int[] GetArrayWithoutDuplicates(int[] array)
+        private static int[] GetArrayAbs100_2(int[] array)
         {
+            return array
+                .Where(x => Math.Abs(x) < 100)
+                .ToArray();
+        }
+
+        /// <summary>
+        /// Задание три.
+        /// </summary>
+        /// <param name="array"></param>
+        /// <returns></returns>
+        private static void GetArrayWithoutDuplicates(int[] array)
+        {
+            Console.WriteLine("\nЗадание три");
             int newArrayLength = array.Length;
             for (int i = 0; i < array.Length; i++)
             {
@@ -48,8 +122,72 @@ namespace ConsoleApp1
                     counter++;
                 }
             }
-            return newArray;
+            PrintArray(newArray);
         }
+
+        /// <summary>
+        /// Задание четыре и пять.
+        /// </summary>
+        private static void GetAndPrintTwoDimensionalArray()
+        {
+            Console.WriteLine("\nЗадание четыре и пять");
+            int[,] array = GetTwoDimensionalArray();
+            PrintArray(array);
+        }
+
+        /// <summary>
+        /// Задание шесть.
+        /// </summary>
+        private static void GetAndPrintTwoDimensionalArray_Line()
+        {
+            Console.WriteLine("\nЗадание шесть");
+            int[,] array = GetTwoDimensionalArray_Line();
+            PrintArray(array);
+        }
+
+        /// <summary>
+        /// Задание семь.
+        /// </summary>
+        private static void GetAndPrintTwoDimensionalArray_EvenLines()
+        {
+            Console.WriteLine("\nЗадание семь");
+            int[,] array = GetTwoDimensionalArray();
+            PrintTwoDimensionalArray_EvenLines(array);
+        }
+
+        /// <summary>
+        /// Задание восемь.
+        /// </summary>
+        private static void GetAndPrintJaggedArray_Console()
+        {
+            Console.WriteLine("\nЗадание восемь");
+            int[][] array = GetJaggedArray();
+            PrintArray(array);
+        }
+
+        /// <summary>
+        /// Задание девять.
+        /// </summary>
+        private static void GetAndPrintJaggedArray_ReplaceEvenValues()
+        {
+            Console.WriteLine("\nЗадание девять");
+            int[][] array = GetJaggedArray();
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int j = 0; j < array[i].Length; j++)
+                {
+                    if (array[i][j] % 2 == 0)
+                    {
+                        array[i][j] = i * j;
+                    }
+                }
+            }
+            PrintArray(array);
+        }
+
+
+
+        #region Helper methods
 
         private static bool Exists(int value, int[] array)
         {
@@ -65,24 +203,140 @@ namespace ConsoleApp1
 
         private static void PrintArray(int[] array)
         {
-            foreach (var item in array)
+            foreach (var element in array)
             {
-                Console.Write(item + " ");
+                Console.Write(element + " ");
             }
             Console.WriteLine();
             Console.WriteLine();
         }
 
-        static int[] GetArray(int arrayLength)
+        private static void PrintArray(int[,] array)
         {
-            Random random = new Random();
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    Console.Write($"{array[i, j]} ");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+            Console.WriteLine();
+        }
+
+        private static void PrintArray(int[][] array)
+        {
+            foreach (var innerArray in array)
+            {
+                foreach (var element in innerArray)
+                {
+                    Console.Write(element + " ");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+            Console.WriteLine();
+        }
+
+        private static void PrintTwoDimensionalArray_EvenLines(int[,] array)
+        {
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    var element = i % 2 == 0
+                        ? array[i, array.GetLength(1) - 1 - j]
+                        : array[i, j];
+                    Console.Write($"{element} ");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+            Console.WriteLine();
+        }
+
+        private static int[] GetArray(int arrayLength)
+        {
             int[] array = new int[arrayLength];
             for (int i = 0; i < arrayLength; i++)
             {
-                int value = random.Next(0, 10);
+                int value = _random.Next(0, 10);
                 array[i] = value;
             }
             return array;
         }
+
+        private static int[,] GetTwoDimensionalArray()
+        {
+            Console.WriteLine("Введите количество строк:");
+            string rows_str = Console.ReadLine();
+            int rows = int.Parse(rows_str);
+
+            Console.WriteLine("Введите количество столбцов:");
+            string columns_str = Console.ReadLine();
+            int columns = int.Parse(columns_str);
+
+            int[,] array = new int[rows, columns];
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    array[i, j] = _random.Next(-100, 101);
+                }
+                Console.WriteLine();
+            }
+            return array;
+        }
+
+        private static int[,] GetTwoDimensionalArray_Line()
+        {
+            Console.WriteLine("Введите количество строк:");
+            string rows_str = Console.ReadLine();
+            int rows = int.Parse(rows_str);
+
+            Console.WriteLine("Введите количество столбцов:");
+            string columns_str = Console.ReadLine();
+            int columns = int.Parse(columns_str);
+
+            int[,] array = new int[rows, columns];
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                Console.WriteLine("Введите элементы строки массива через пробел:");
+                string[] valuesInLine = Console.ReadLine().Split();
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    array[i, j] = int.Parse(valuesInLine[j]);
+                }
+                Console.WriteLine();
+            }
+            return array;
+        }
+
+        private static int[][] GetJaggedArray()
+        {
+            Console.WriteLine("Введите количество строк:");
+            string rows_str = Console.ReadLine();
+            int rows = int.Parse(rows_str);
+
+            int[][] array = new int[rows][];
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine("Введите количество столбцов:");
+                string columns_str = Console.ReadLine();
+                int columns = int.Parse(columns_str);
+                array[i] = new int[columns];
+
+                Console.WriteLine($"Введите построчно элементы массива {i + 1}:");
+                for (int j = 0; j < array[i].Length; j++)
+                {
+                    array[i][j] = int.Parse(Console.ReadLine());
+                }
+                Console.WriteLine();
+            }
+            return array;
+        }
+
+        #endregion
     }
 }
