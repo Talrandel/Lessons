5d54b6aa069f4d7231adc930fb1e35d514a1ef2e
Eugene Makarov <makarovevgeniy7a@gmail.com>
2024-02-27 17:57:55 +0300

    exceptions

---
+++ Classes.Exceptions/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Classes.Exceptions;
+
+class Program
+{
+    static void Main()
+    {
+        int[] someArray = null;
+
+        for (int i = 0; i < 10; i++) 
+        {
+            someArray[i] = i;
+            Console.WriteLine(someArray[i]);
+        }
+
+    }
+}
