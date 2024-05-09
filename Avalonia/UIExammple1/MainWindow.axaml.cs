84d966f2a10647cf8db68645b852a32408f3e934
Eugene Makarov <makarovevgeniy7a@gmail.com>
2024-05-09 15:53:38 +0300

    Some ui example

---
+++ Avalonia/UIExammple1/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace UIExammple1
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
