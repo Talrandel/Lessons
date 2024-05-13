0ba0e623935895e895b2e4462dfc1feab219cfe8
Eugene Makarov <makarovevgeniy7a@gmail.com>
2024-05-13 21:36:30 +0300

    hw practice

--- Avalonia/MvvmExample/ViewModels/MainWindowViewModel.cs
+++ Avalonia/MvvmExample/ViewModels/MainWindowViewModel.cs
@@ -6,12 +6,15 @@ public class MainWindowViewModel : ViewModelBase
 {
     public MainWindowViewModel()
     {
-        Books = new List<Book>()
-        {
+        Books =
+        [
             new("Гарри Поттер", "Джоан Роулинг"),
             new("Мёртвые души", "Николай Гоголь"),
             new("Евгений Онегин", "Александр Пушкин"),
-        };
+            new("Толковый словарь Даля", "В. И. Даль"),
+            new("Вредные советы", "Григорий Остер"),
+            new("Чистый код", "Роберт Мартин"),
+        ];
     }
 
     public List<Book> Books { get; set; }
